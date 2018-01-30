52eba9b694deae28a8c975e9cfe557e069825243
ronxy_huang <sixin_huang@outlook.com>
2018-01-30 13:42:27 +0800

    APP-10564:sign-wall
    Desc :  更新签到操作页面以及3d
    Impact : 更新签到操作页面以及3d

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,86 +1,39 @@
 <template>
-    <div class="bg">
-        <div class="container">
-            <swiper :options="swiperOption">
-                <swiper-slide>
-                    <div class="item">
-                        <img src="../../public/assets/img/test.jpeg" />
-                        <div class="team">支付产品</div>
-                        <div class="name">name</div>
-                    </div>
-                    <div class="item">
-                        <img src="../../public/assets/img/test.jpeg" />
-                        <div class="name">name</div>
-                    </div>
-                    <div class="item">
-                        <img src="../../public/assets/img/test.jpeg" />
-                        <div class="name">name</div>
-                    </div>
-                    <div class="item">
-                        <img src="../../public/assets/img/test.jpeg" />
-                        <div class="name">name</div>
-                    </div>
-                    <div class="item">
-                        <img src="../../public/assets/img/test.jpeg" />
-                        <div class="name">name</div>
-                    </div>
-                    <div class="item">
-                        <img src="../../public/assets/img/test.jpeg" />
-                        <div class="name">name</div>
-                    </div>
-                </swiper-slide>
-                <!-- Optional controls -->
-            </swiper>
-        </div>
-
+    <div id="container">
     </div>
 </template>
 
 <script>
   import axios from 'axios';
-  import { swiper, swiperSlide } from 'vue-awesome-swiper'
-  import { ModelObj } from 'vue-3d-model'
+
   import getQueryString from '../utils/utils';
-  import 'swiper/dist/css/swiper.css'
   import '../stlye/list.less';
 
   export default {
     components: {
-      swiper,
-      swiperSlide
-      ModelObj,
+
     },
     data() {
       return {
-        swiperOption: {
-//          slidesPerView: 2,
-//          slidesPerColumn: 6,
-//          spaceBetween: 10,
-          loop: true,
-//          autoplay: {
-//            delay: 2500,
-//            disableOnInteraction: false
-//          },
-          pagination: {
-            el: '.swiper-pagination',
-            clickable: true
-          },
-        },
         count: 0,
         list: {},
         code: '',
         state: '',
         show: false,
+        table: [
+          "Lv", "Livermorium", "(293)", 16, 7,
+          "Ts", "Tennessine", "(294)", 17, 7,
+          "Og", "Oganesson", "(294)", 18, 7,
+          "aaaa", "Oganesson", "(294)", 10,11
+        ]
       };
     },
     created: function () {
-      this.code = getQueryString('code');
-      this.state = getQueryString('state');
+
     },
     methods: {
       getMemberList() {
-        console.log(this.show);
-        this.show = !this.show;
+
       },
     }
 
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,66 +1,162 @@
 <template>
-    <div class="bg" id="register">
-        <div class="container" >
-            <img class="user" src="../../public/assets/img/test.jpeg" alt="微信头像">
-            <input class="name" value="微信用户"/>
-            <mt-picker :slots="slots" @change="onValuesChange" class="team"></mt-picker>
-            <mt-button class="confirm" size="large" @click="submitSign">确认签到</mt-button>
+    <div class="bg" id="register" >
+        <img src="../../public/assets/img/bg2@2x.png" style="width: 100%;margin-top: 36px"/>
+        <div class="container" v-if="!isLoading">
+            <img class="user" :src="headImgUrl" alt="">
+            <div class="wxname">{{wxName}}</div>
+            <div class="formItem">
+                <img src="../../public/assets/img/icon_name@2x.png" class="icon">
+                <input class="name" v-model="name" placeholder="请输入您的大名" maxlength="5" />
+            </div>
+            <div class="formItem" @click="toggleActionSheet" style="margin-top: 10px">
+                <img src="../../public/assets/img/icon_dept.@2x.png" class="icon">
+                    <div class="dept" v-if="organizationText !== '请选择所在部门'">{{organizationText}}</div>
+                    <div class="deptHolder" v-else>{{organizationText}}</div>
+
+                <img class="arrow" src="../../public/assets/img/arrow@2x.png" />
+            </div>
+
+            <button class="confirm" @click="submitSign"></button>
+
         </div>
+        <transition name="fade">
+            <div class="modal" v-show="showModal" @click="toggleModal">
+                <img src="../../public/assets/img/close@2x.png" class="close" />
+                <img src="../../public/assets/img/success.png" class="success">
+            </div>
+        </transition>
+        <actionsheet :actions="actions" v-model="actionShow"></actionsheet>
 
     </div>
 </template>
 
 <script>
   import axios from 'axios';
-  import { Cell, Checklist,Field, Button, MessageBox, Indicator,Picker } from 'mint-ui';
+  import { Toast, MessageBox, Indicator,Picker, Actionsheet } from 'mint-ui';
   import getQueryString from '../utils/utils';
+  import bg from '../../public/assets/img/bg1@2x.png'
+  import defalut from '../../public/assets/img/weixin.png'
   import '../stlye/register.less';
 
   export default {
     components: {
-      Cell,
-      Field,
-      'mtButton': Button,
-      'mtCell': Cell,
       'mtPicker': Picker,
+      Actionsheet,
     },
     data() {
       return {
-        count: 0,
-        list: {},
+        isLoading: false,
         code: '',
         state: '',
+        accessToken: '',
+        openId: '',
+        organizationText: '请选择所在部门',
+        organization: null,
+        name: '',
+        headImgUrl: defalut,
+        wxName: '默认',
         show: false,
-        slots: [
-          {
-            flex: 1,
-            values: ['支付产品', '金融','产品'],
-            className: 'slot1',
-            textAlign: 'center'
-          }
-        ]
-      };
+        actionShow: false,
+        showModal: false,
+        actions: [{name:'支付产品中心',method: () => this.selectActionSheet('支付产品中心','支付产品中心')}, {name: '技术研发中心', method:
+            () => this.selectActionSheet('技术研发中心', '技术研发中心')},
+          {name: '金融商城中心', method: () => this.selectActionSheet('金融商城中心', '金融商城中心')}],
+        userInfo: {}
+    };
     },
     created: function () {
-//      Indicator.open({
-//        text: '加载中',
-//        spinnerType: 'fading-circle'
-//      });
+
+      this.isLoading = true;
+      Indicator.open({
+        text: '加载中',
+        spinnerType: 'fading-circle'
+      });
+
       this.code = getQueryString('code');
       this.state = getQueryString('state');
+
+      const url = 'https://ebd.99bill.com/mkt-express/wx/oauthInfo';
+      const headers = {
+        'Content-Type': 'application/json',
+      };
+
+      const data = {
+        bindSource: 'H5test',
+        code: '071dEWfF1Pkyz00FuziF1NvBfF1dEWfg',
+      };
+
+      const _self = this;
+
+      axios({method: 'post', url: url ,headers: headers, data: data}).then((res)=> {
+        console.log(res);
+        const data = res.data;
+        if(data.errCode === '00') {
+          _self.openId = data.openid;
+          _self.name = data.name;
+          _self.headImgUrl = data.headimgurl;
+          _self.wxName = data.nickname;
+          _self.userInfo = data;
+          _self.isLoading = false;
+          Indicator.close();
+        } else {
+          Toast(`请求错误:${res.errMsg}`);
+        }
+      }).catch(function(error) {
+        Toast(`请求失败:${error}`);
+      });
     },
     methods: {
-      onValuesChange() {
+      toggleActionSheet() {
+        this.actionShow = !this.actionShow
+      },
 
+      selectActionSheet(value,text) {
+        this.organization = value;
+        this.organizationText = text;
+      },
+
+      toggleModal() {
+        this.showModal = !this.showModal;
       },
 
       submitSign() {
-        MessageBox.confirm('信息都确认清楚了吗').then(action => {
-            console.log(213);
+        if (!this.name) {
+          Toast('请输入姓名...');
+          return;
+        }
+
+        if (!this.organization) {
+          Toast('请选择部门...');
+          return;
+        }
+        MessageBox.confirm('信息都确认清楚了吗？').then(action => {
+            if(action === 'confirm') {
+              const url = 'https://ebd.99bill.com/mkt-express/mkt/signWall/save';
+              const data = {headImgUrl: this.headImgUrl, name: this.name, openId: this.openId, organization:
+              this.organization,signWallCode: this.state || 'test_wall_2', wxName: this.wxName};
+              const headers = {
+                'Content-Type': 'application/json',
+              };
+              let _self =this;
+              axios({url:url, method: 'post', data: data,headers: headers}).then(
+                function(res) {
+                  const data = res.data;
+                  if(data.errCode === '00') {
+                    _self.showModal = true;
+                    console.log('添加成功');
+                  } else {
+                    Toast(`请求错误:${res.errMsg}`);
+                  }
+                }
+              ).catch(function (error) {
+                Toast(`请求失败:${error}`);
+              });
+            } else {
+
+            }
+
         });
-        const url = '';
-        const params = '';
-        axios().then().catch();
+
       }
     }
 
